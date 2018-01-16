62819fa54a7f170cadf2799d163f1495325271bd
devil1993 <samanwayghatak@gmail.com>
2018-01-16 18:51:47 +0530

    Unknown changes

--- settings.py
+++ settings.py
@@ -1,22 +1,11 @@
 folders = [
-'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByStudents/Date_26_10_17/DATA_10_07_56/All/'
-# '/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_22_09_17ukhra/DATA_07_45_35/All/UP_c/'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_22_09_17ukhra/DATA_16_10_18/All/UP'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_22_09_17ukhra/DATA_10_32_14/All/UP'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_22_09_17ukhra/DATA_07_15_36/All/UP'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_21_09_17ukhra/DATA_16_16_10/All/UP'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_20_09_17ukhra/DATA_06_50_30/All/U'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_20_09_17ukhra/DATA_07_27_24/All/U'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_20_09_17ukhra/DATA_07_29_45/All/U'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_20_09_17ukhra/DATA_07_33_29/All/U'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_20_09_17ukhra/DATA_10_31_09/All/U'
-# ,'/home/baba/Dropbox/RoadSegmentBehaviourAnalysis/DataSet/ByAbhijit/Bus/Date_20_09_17ukhra/DATA_17_12_44/All/U_C'
+	# folders to be included
 ]
-out_folder = './Results/Bus_Ukhra_us_Up_Trail/'
+out_folder = 'Results'
 config = {
-	"ground_truth_file" : "ukhramarged"
-	,"stop_length": 60
-	,"output_file_prefix": "ukhra_result_"
+	"ground_truth_file" : "ground_truth"
+	,"stop_length": 52
+	,"output_file_prefix": "_result_"
 }
 threshold_speed = 2.5
 sound_threshold = 92
